259a5714985e96ae2233717f5486642697c056cb
Abel <abeldawit.h@gmail.com>
2023-06-02 10:12:48 +0000

    fixting error for task 1323

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 
 /**
+ * @array_to_heap - builds a Max Binary Heap tree from an array.
  * @array: pointer to the first element of the array to be converted
  * @size: number of elements in the array
  * Return: pointer to the root node of the created Binary Heap,
