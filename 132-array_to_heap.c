353201d59e10ea2259a6a4186a961ffcd260e2c9
Abel <abeldawit.h@gmail.com>
2023-06-02 09:58:20 +0000

    udating advanced tasks errors

---
+++ 132-array_to_heap.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of elements in the array
+ * Return: pointer to the root node of the created Binary Heap,
+ * or NULL on failure
+ */
+heap_t *array_to_heap(int *array, size_t size)
+{
+	size_t i;
+	heap_t *root = NULL;
+
+	if (!array)
+		return (NULL);
+
+	root = heap_insert(&root, array[0]);
+	for (i = 1; i < size; i++)
+		heap_insert(&root, array[i]);
+
+	return (root);
+}
